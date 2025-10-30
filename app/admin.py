32a86326763cd47fd3899eb194680444244366db
Faizan Abid <faizanabid1038@gmail.com>
2025-10-30 22:04:43 +0500

    SecondBranch

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from app.models import CustomUser,IPRequestLog
 # IPRequestLog
 
-# Register your models here.
+
 admin.site.register(CustomUser)
-# admin.site.register(IPRequestLog)
+
 admin.site.register(IPRequestLog)
